a23764d109127cb2d3e949d7f611844ba677f518
curie3170 <curie3170@naver.com>
2022-08-24 23:08:26 +0900

    change focal loss

--- losses/losses.py
+++ losses/losses.py
@@ -34,7 +34,7 @@ class FocalLoss(nn.Module):
         self.reduce = reduce
 
     def forward(self, inputs, targets):
-        ce_loss = nn.CrossEntropyLoss()(inputs, targets)
+        ce_loss = nn.CrossEntropyLoss(reduction='none')(inputs, targets)
 
         pt = torch.exp(-ce_loss)
         F_loss = self.alpha * (1-pt)**self.gamma * ce_loss
